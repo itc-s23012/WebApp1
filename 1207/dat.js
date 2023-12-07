78c485ecd23fd53db3466d5ac11ebd7d15ae0297
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-12-07 07:49:57 +0000

    1207/paiza

--- 1207/dat.js
+++ 1207/dat.js
@@ -1,11 +1,6 @@
-const print = console.log
-const stream = require('fs')
-  //  .readFileSync('/dev/stdin', 'utf-8') //
-  .readFileSync('./data', 'utf8')
-  .trim()
+const file = '/dev/stdin'
+// const file = './data'
+const stream = require('fs').readFileSync(file, 'utf8').trim()
 const lines = stream.split('\n')
-const [a, b] = lines
 
-print('stream', stream)
-print('lines', lines)
-print(a, b)
+console.log(lines[0])
