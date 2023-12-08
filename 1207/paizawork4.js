69c3bc5e808c3bf8bd39ea1c79254254619cfebf
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-12-08 02:45:11 +0000

    paizawork4.js/N行のデータ入力

--- 1207/paizawork4.js
+++ 1207/paizawork4.js
@@ -3,6 +3,7 @@ const stream = require('fs')
   .readFileSync('./data', 'utf8')
   .trim()
 const lines = stream.split('\n')
-const N = parseInt(lines[0], 10)
-const select = lines.slice(1, N + 1)
-console.log(select.join('\n'))
+// const N = parseInt(lines[0], 10) //
+//const select = lines.slice(1, N + 1) //
+// console.log(select.join('\n')) //
+console.log(lines.slice(1).join('\n'))
