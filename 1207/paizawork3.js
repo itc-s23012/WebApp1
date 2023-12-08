118cab99fcc975cdbb03f06698b60e9d9007eb20
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-12-08 03:02:18 +0000

    paizawork3.js/3行のデータ

--- 1207/paizawork3.js
+++ 1207/paizawork3.js
@@ -4,6 +4,7 @@ const stream = require('fs')
   .trim()
 const lines = stream.split('\n')
 
-console.log(lines[0])
-console.log(lines[1])
-console.log(lines[2])
+//console.log(lines[0]) //
+//console.log(lines[1]) //
+//console.log(lines[2]) //
+console.log(lines.join('\n'))
