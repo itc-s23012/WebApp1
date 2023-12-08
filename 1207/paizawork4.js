aed42d77e484be6ba05c8ea7a76bdd1c777cf029
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-12-08 02:35:12 +0000

    paizawork4.js/N行のデータ

--- 1207/paizawork4.js
+++ 1207/paizawork4.js
@@ -3,4 +3,6 @@ const stream = require('fs')
   .readFileSync('./data', 'utf8')
   .trim()
 const lines = stream.split('\n')
-const [_, ...rest]
+const N = parseInt(lines[0], 10)
+const select = lines.slice(1, N + 1)
+console.log(select.join('\n'))
